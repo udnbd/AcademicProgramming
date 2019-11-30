59d00831b6bef9392169dc54d3979cf3d9fed087
Ibrahim Sharif <shuvoaftab@gmail.com>
2019-12-01 02:16:17 +0600

    New Codes

--- Algorithm/MetroUni/Practise/BFSDFS-AB.c
+++ Algorithm/MetroUni/Practise/BFSDFS-AB.c
@@ -1,19 +1,18 @@
-//#ifndef
-//Queue_h
-//#define Queue_h
 #include <stdlib.h>
 #include <stdio.h>
+
 struct Node
 {
     int data;
     struct Node *next;
 } *front = NULL, *rear = NULL;
+
 void enqueue(int x)
 {
     struct Node *t;
     t = (struct Node *)malloc(sizeof(struct Node));
     if (t == NULL)
-        printf("Queue is FUll\n");
+        printf("Queue is Full\n");
     else
     {
         t->data = x;
@@ -31,6 +30,7 @@ int dequeue()
 {
     int x = -1;
     struct Node *t;
+
     if (front == NULL)
         printf("Queue is Empty\n");
     else
@@ -42,17 +42,21 @@ int dequeue()
     }
     return x;
 }
-int isEmpty() { return front == NULL; }
-//#endif /* Queue_h */
 
-//#include <Queue.h>
+int isEmpty()
+{
+    return front == NULL;
+}
+
 void BFS(int G[][7], int start, int n)
 {
     int i = start, j;
     int visited[7] = {0};
+
     printf("%d ", i);
     visited[i] = 1;
     enqueue(i);
+
     while (!isEmpty())
     {
         i = dequeue();
@@ -72,10 +76,12 @@ void DFS(int G[][7], int start, int n)
 {
     static int visited[7] = {0};
     int j;
+
     if (visited[start] == 0)
     {
         printf("%d ", start);
         visited[start] = 1;
+
         for (j = 1; j < n; j++)
         {
             if (G[start][j] == 1 && visited[j] == 0)
@@ -83,6 +89,7 @@ void DFS(int G[][7], int start, int n)
         }
     }
 }
+
 int main()
 {
     int G[7][7] = {{0, 0, 0, 0, 0, 0, 0},
@@ -92,8 +99,10 @@ int main()
                    {0, 0, 1, 1, 0, 1, 1},
                    {0, 0, 0, 0, 1, 0, 0},
                    {0, 0, 0, 0, 1, 0, 0}};
+
     BFS(G, 4, 7);
     printf("\n");
     DFS(G, 4, 7);
+
     return 0;
 }
--- Algorithm/MetroUni/main.cpp
+++ Algorithm/MetroUni/main.cpp
@@ -1,11 +1,38 @@
-#include <cstdio>
+#include <stdio.h>
 
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int knapsack(int cap, int wght[], int prof[], int n)
+{
+    int i, j;
+    int knap[n + 1][cap + 1];
+    for (i = 0; i <= n; i++)
+        for (j = 0; j <= cap; j++)
+        {
+            if (i == 0 || j == 0)
+                knap[i][j] = 0;
+            else if (wght[i - 1] <= j)
+            {
+                knap[i][j] = max(prof[i - 1] + knap[i - 1][j - wght[i - 1]], knap[i - 1][j]);
+            }
+            else
+                knap[i][j] = knap[i - 1][j];
+        }
+    return knap[n][cap];
+}
 int main()
 {
-    int a, b, sum;
-    scanf("%d", &a);
-    b = 10;
-    sum = a + b;
+    int profit[] = {60, 100, 120};
+    int weight[] = {10, 20, 30};
+    int capacity = 50;
 
-    printf("Sum is : %d \n", sum);
+    int n = sizeof(profit) / sizeof(profit[0]);
+    printf("The maximum profit by Knapsack is : %d", knapsack(capacity, weight, profit, n));
+    return 0;
 }
