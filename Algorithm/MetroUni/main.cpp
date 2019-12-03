fe22ce3749b173dde41b1bc29626d88a97d9fb62
Ibrahim Sharif <shuvoaftab@gmail.com>
2019-12-03 17:31:55 +0600

    updates

--- Algorithm/MetroUni/main.cpp
+++ Algorithm/MetroUni/main.cpp
@@ -1,62 +1,27 @@
-#include <stdio.h>
+#include <cstdio>
 
-int max(int a, int b)
-{
-    if (a > b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
-}
+#define I 99999
 
-int knapsack(int capacity, int weight[], int profit[], int n)
-{
-    int i, j;
-    int knap[n + 1][capacity + 1];
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= capacity; j++)
-        {
-            if (i == 0 || j == 0)
-                knap[i][j] = 0;
-            else if (weight[i] <= j)
-                knap[i][j] = max(profit[i] + knap[i - 1][j - weight[i]], knap[i - 1][j]);
-            else
-                knap[i][j] = knap[i - 1][j];
-        }
-    }
-    return knap[n][capacity];
-}
+using namespace std;
+
+int cost[][8] = {{I, I, I, I, I, I, I, I},
+                 {I, I, 25, I, I, I, 5, I},
+                 {I, 25, I, 12, I, I, I, 10},
+                 {I, I, 12, I, 8, I, I, I},
+                 {I, I, I, 8, I, 16, I, 14},
+                 {I, I, I, I, 16, I, 20, 18},
+                 {I, 5, I, I, I, 20, I, I},
+                 {I, I, 10, I, 14, 18, I, I}};
+
+int near[8] = {I, I, I, I, I, I, I, I};
+int t[2][6];
 
 int main()
 {
 
-    int n, i, j, profit[100], weight[100], capacity;
+    int i, j, k, u, v, n = 7, min = 7;
+    for (i = 1; i <= n; i++)
+        for (j = i; j <= n)
 
-    while (1)
-    {
-        printf("\nEnter Number of Products: ");
-        scanf("%d", &n);
-
-        printf("Enter Weight & Profits accordingly Now: \n");
-        for (i = 1; i <= n; i++)
-        {
-            printf("Weight for Product Number %d : ", i);
-            scanf("%d", &weight[i]);
-            printf("Profit for Product Number %d : ", i);
-            scanf("%d", &profit[i]);
-        }
-
-        weight[0] = 0;
-        profit[0] = 0;
-
-        printf("Enter the capacity of Knapsack : ");
-        scanf("%d", &capacity);
-
-        printf("The maximum profit by knapsack is : %d \n", knapsack(capacity, weight, profit, n));
-    }
-    return 0;
+            return 0;
 }
