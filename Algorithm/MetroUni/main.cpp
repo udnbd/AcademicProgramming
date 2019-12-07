083402de82ed0cedff5ea9b2da974ccd5f781a65
Ibrahim Sharif <shuvoaftab@gmail.com>
2019-12-07 18:03:26 +0600

    update

--- Algorithm/MetroUni/main.cpp
+++ Algorithm/MetroUni/main.cpp
@@ -12,12 +12,12 @@ int cost[][8] = {{I, I, I, I, I, I, I, I},
                  {I, I, I, I, 16, I, 20, 18},
                  {I, 5, I, I, I, 20, I, I},
                  {I, I, 10, I, 14, 18, I, I}};
-
 int near[8] = {I, I, I, I, I, I, I, I};
 int t[2][6];
 
 int main()
 {
+
     int i, j, k, u, v, n = 7, min = I;
 
     for (i = 1; i <= n; i++)
@@ -32,19 +32,21 @@ int main()
             }
         }
     }
-
     t[0][0] = u;
     t[1][0] = v;
     near[u] = near[v] = 0;
 
     for (i = 1; i <= n; i++)
-        if (near != 0)
+    {
+        if (near[i] != 0)
         {
             if (cost[i][u] < cost[i][v])
+
                 near[i] = u;
             else
                 near[i] = v;
         }
+    }
 
     for (i = 1; i < n - 1; i++)
     {
@@ -60,6 +62,7 @@ int main()
 
         t[0][i] = k;
         t[1][i] = near[k];
+        near[k] = 0;
 
         for (j = 1; j <= n; j++)
         {
@@ -70,5 +73,6 @@ int main()
 
     for (i = 0; i < n - 1; i++)
         cout << "(" << t[0][i] << "," << t[1][i] << ")" << endl;
+
     return 0;
 }
