dcc80b3800e265d7d3cdc5e228b3d09dd6d06f5b
Ibrahim Sharif <shuvoaftab@gmail.com>
2019-12-24 18:01:59 +0600

    4 Nested Loop Test

---
+++ SelfPractise/Schaum's Outline/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k, l;
+    int n = 5;
+    char val = '0';
+
+    for (i = 0; i < n; i++)
+    {
+        printf("\n");
+        printf("Level %d ->", i + 1);
+        printf("\n");
+        for (j = 0; j < n; j++)
+        {
+            //printf("Level J");
+            printf("\n");
+            for (k = 0; k < n; k++)
+            {
+                //printf("Level K");
+                printf("\n");
+                for (l = 0; l < n; l++)
+                {
+                    printf("%c ", val);
+                }
+            }
+        }
+    }
+    return 0;
+}
